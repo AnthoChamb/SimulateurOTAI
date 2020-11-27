32f360c917dbe955294247beb5c2da231abe1b4d
Anthony Chamberland <61058826+AnthoChamb@users.noreply.github.com>
2020-11-27 11:29:39 -0500

    Squelette scénario de Oli
    Squelette des classes, données membres, accesseurs et méthodes de la section Scénario (Sans code à l'intérieur).
    
    Co-authored-by: gloskrrr <63416501+gloskrrr@users.noreply.github.com>

---
+++ Scenario/ControleurScenario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTAI.Scenario {
+    public class ControleurScenario {
+        #region Données membres
+
+        private FormScenario interfaceScenario;
+        private Scenario scenario;
+
+        #endregion
+
+        #region Constructeurs
+
+        public ControleurScenario() {
+
+        }
+
+        #endregion
+
+        #region Méthodes publiques
+
+        public void AjouterAeroport(string nom, Position position, int minPassagers, int maxPassagers, int minMarchandise, int maxMarchandise) {
+
+        }
+
+        public void AjouterVehicule(int Aeroport, TypeVehicule typeVehicule, string nom, int vitesse, int? embarquement, int? debarquement, int? entretien, object? capacite) {
+
+        }
+
+        public string SelectionnerPosition() {
+            return "";
+        }
+
+        public void EnregistrerScenario(string chemin) {
+
+        }
+
+        #endregion
+
+        #region Propriétés publiques
+
+
+        #endregion
+
+    }
+}
