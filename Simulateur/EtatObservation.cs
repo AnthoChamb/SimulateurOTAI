3003149dc883bb049b49001149615dae7df4d431
Anthony Chamberland <61058826+AnthoChamb@users.noreply.github.com>
2020-12-04 14:53:57 -0500

    Codification de la simulation des vols

--- Simulateur/EtatObservation.cs
+++ Simulateur/EtatObservation.cs
@@ -3,17 +3,43 @@
 namespace OTAI.Simulateur {
     /// <summary>Classe d'un état d'observation</summary>
     public class EtatObservation : EtatVol {
+        const float DISTANCE = 2;
+
         /// <summary>Crée un état d'observation</summary>
         /// <param name="destination">Position de destination de l'observation</param>
         /// <param name="vitesse">Vitesse du véhicule en observation</param>
-        public EtatObservation(Position destination, int vitesse) : base(destination, destination, vitesse) { }
+        public EtatObservation(Position destination, int vitesse) : base(vitesse) {
+            this.destination = destination;
+
+            Position origine = destination;
+            origine.Lon -= DISTANCE;
+
+            this.origine = position = origine;
+
+            angle = Math.Atan(origine.Lon - destination.Lon / origine.Lat - destination.Lat);
+        }
 
         /// <summary>Obtient la position actuelle du véhicule en observation</summary>
-        public override Position Position { get => throw new NotImplementedException(); }
+        public override Position Position {
+            get {
+                float lat, lon;
+                double rad = ConvertirDegRad(((position.Lon - origine.Lon) * 360) / DISTANCE);
+
+                lat = (float)Math.Sin(rad);
+                lon = (float)Math.Cos(rad);
+
+                return new Position(lat, lon);
+            }
+        }
 
         /// <summary>Obtient une représentation en chaine de l'état ainsi que du temps écoulé depuis le début de l'observation</summary>
         /// <returns>Retourne une représentation en chaine de l'état ainsi que du temps écoulé depuis le début de l'observation</returns>
         /// <remarks>Le temps est retourné sous le format <c>hh:mm:ss</c></remarks>
         public override string ToString() => "En observation - " + base.ToString();
+
+        /// <summary>Converti un angle en degré en valeur radian</summary>
+        /// <param name="deg">Angle en degré</param>
+        /// <returns>Retourne la valeur en radian d'un angle en degré</returns>
+        private double ConvertirDegRad(double deg) => Math.PI * deg / 100;
     }
 }
--- Simulateur/EtatVol.cs
+++ Simulateur/EtatVol.cs
@@ -3,20 +3,32 @@
 namespace OTAI.Simulateur {
     /// <summary>Classe d'un état de vol</summary>
     public class EtatVol : Etat {
-        private readonly Position origine;
-        protected readonly Position destination;
-        protected Position position;
+        #region Données mmebres
+
+        protected Position origine, position, destination;
         protected readonly int vitesse;
+        protected double angle;
+
+        #endregion
+
+        #region Constructeurs
+
+        /// <summary>Constructeur de base d'un état de vol</summary>
+        /// <param name="vitesse">Vitesse du véhicule en vol</param>
+        protected EtatVol(int vitesse) => this.vitesse = vitesse;
 
         /// <summary>Crée un état de vol</summary>
         /// <param name="origine">Position d'origine du vol</param>
         /// <param name="destination">Position de destination du vol</param>
         /// <param name="vitesse">Vitesse du véhicule en vol</param>
-        public EtatVol(Position origine, Position destination, int vitesse) {
+        public EtatVol(Position origine, Position destination, int vitesse) : this(vitesse) {
             this.origine = position = origine;
             this.destination = destination;
-            this.vitesse = vitesse;
+
+            angle = Math.Atan(origine.Lon - destination.Lon / origine.Lat - destination.Lat);
         }
+
+        #endregion
 
         #region Propriétés publiques
 
@@ -36,7 +48,13 @@ namespace OTAI.Simulateur {
         public override void Simuler(int temps) {
             base.Simuler(temps);
 
-            throw new NotImplementedException();
+            while (position > destination == origine > destination && temps > 0) {
+                position.Lon += (float)Math.Cos(angle) * vitesse;
+                position.Lon += (float)Math.Sin(angle) * vitesse;
+            }
+
+            if (temps > 0)
+                LeverFinEtat(this, temps);
         }
 
         /// <summary>Obtient une représentation en chaine de l'état ainsi que du temps écoulé depuis le début du vol</summary>
